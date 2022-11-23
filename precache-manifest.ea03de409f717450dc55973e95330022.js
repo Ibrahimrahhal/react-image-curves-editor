8bcae6129fbc6b70689b3b1a0d0b109818ee6f7d
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2022-11-23 23:18:39 +0300

    Updates

---
+++ precache-manifest.ea03de409f717450dc55973e95330022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb32646ca6ad66c13051ba2d14c9f065",
+    "url": "./index.html"
+  },
+  {
+    "revision": "775e82125f4e84d008bb",
+    "url": "./static/css/main.48b58499.chunk.css"
+  },
+  {
+    "revision": "fbc7e3fb7b2e9ff2a052",
+    "url": "./static/js/2.0d0b3373.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0d0b3373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775e82125f4e84d008bb",
+    "url": "./static/js/main.51c5de9a.chunk.js"
+  },
+  {
+    "revision": "eb0e2789e296a46670b0",
+    "url": "./static/js/runtime-main.8721eb49.js"
+  }
+]);
